d58d69685f0f1af4e111715eed347a5a0e83a22a
Mentikora <notender_boy@ukr.net>
2016-08-23 03:27:02 +0300

    form height by js -> commented

--- js/appication.js
+++ js/appication.js
@@ -2,13 +2,13 @@
 
 $(function() {
 
-  //menu switcher
+  // menu switcher
 	$('.menu_switch').on('click', function(){
 		$(this).toggleClass('active');
 		$('#navigation').toggleClass('open');
 	});
 
-	//menu navigation li dropdown
+	// menu navigation li dropdown
 	$('.is_dropdown').on('click', function(){
 		$('.nav_dropdown').toggleClass('open');
 	});
@@ -16,6 +16,10 @@ $(function() {
 	// product image
 	$(window).on("load resize", function(){
 	  $('#product_img').height( $('#product_img').width() );
+
+	  // mediaquery product form height
+		// if (window.matchMedia("(min-width: 991px)").matches) {
+	 //  	$('.product_form').height($('#product_img').height() - 10);
+		// }
 	});
-	
 });
